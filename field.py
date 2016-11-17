74f1a39f0a2fa0aea153cefe98cfe6bb064a5675
sashaaero <sashaaero@yandex.ru>
2016-11-17 14:59:20 +0300

    field -> class

--- field.py
+++ field.py
@@ -1,66 +1,83 @@
 from pprint import pprint
+from cell import Cell
+from PyQt5.QtWidgets import QWidget
 
+class Field(QWidget):
 
-def create_field(i, j, fill_with=None):
-    """
-    Creates a field with size of given i, j arguments [i, j]
-    :param i: number of rows
-    :param j: number of columns
-    :param fill_with: object that will be placed in cells
-    :return: field (double dimensioned list)
-    """
-    field = []
-    for x in range(i):
-        field.append(list())
-        for y in range(j):
-            field[x].append(fill_with)
+    def __init__(self, size=9):
+        self.fsize = size
+        self.field = self._create_field()
+        self._generate_field()
 
-    return field
+    def print(self):
+        for row in self.field:
+            print('\t'.join(map(str, row)))
+            print()
 
+    def _create_field(self):
+        """
+        Creates a field with size of given size
+        :return: field (double dimensioned list)
+        """
+        field = []
+        for x in range(self.fsize):
+            field.append(list())
+            for y in range(self.fsize):
+                field[x].append(Cell())
 
-def put_line_in_line(line1, line2):
-    """
-    Put values from line1 into line1 in place
-    :param line1: line that should be edited
-    :param line2: values for line1
-    :return: None
-    """
+        return field
 
-def shifted_line(line, num):
-    """
-    Shifts line by given number of indexes
-    :param line: line that will be shifted
-    :param num: number of indexes that will shift
-    :return shifted_line: shifted line
-    """
-    shifted_line_ = []
-    for i in range(len(line)):
-        shifted_line_.append(line[(i + num) % len(line)])
+    @staticmethod
+    def _put_line_in_line(line1, line2):
+        """
+        Put values from line1 into line1 in place
+        :param line1: line that should be edited
+        :param line2: values for line1
+        :return: None
+        """
+        assert len(line1) == len(line2)
+        for i in range(len(line1)):
+            line1[i] = line2[i]
 
+    @staticmethod
+    def _shifted_line(line, num):
+        """
+        Shifts line by given number of indexes
+        :param line: line that will be shifted
+        :param num: number of indexes that will shift
+        :return shifted_line: shifted line
+        """
+        shifted_line = []
+        for i in range(len(line)):
+            shifted_line.append(line[(i + num) % len(line)])
 
+        return shifted_line
 
+    def _transpose(self):
+        for i in range(self.fsize):
+            for j in range(self.fsize):
+                self.field[i][j], self.field[j][i] = self.field[j][i], self.field[i][j]
 
-def generate_field(size=9, district_size=3):
-    """
-    Generates a field for a game
-    :param size: size of a field [size x size]
-    :param district_size: @See documentation for meaning of district
-    :return field: game field
-    """
-    raise_line = list(range(1, 10))
-    field = create_field(size, size)
-    for district in range(size / district_size):
-        for line in range(district_size):
-            put_line_in_line(field[line * district],
+    def _generate_field(self):
+        """
+        Generates a field for a game
+        :return field: game field
+        """
+        district_size = self.fsize // 3
 
+        def calc_shift(n):
+            return (n // district_size) + district_size * (n % district_size)
 
+        for i in range(len(self.field)):
+            ins_line = self._shifted_line(list(range(1, 10)), calc_shift(i))
+            self._put_line_in_line(self.field[i], ins_line)
+
+        self._transpose()
 
 
 
 def main():
-    field = create_field(9, 9, 1)
-    pprint(field)
-
-
+    field = Field()
+    field.print()
 
 main()
