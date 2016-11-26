548481789cf6378c29b95a194e33796b34b1e474
sashaaero <sashaaero@yandex.ru>
2016-11-26 23:18:17 +0300

    operations

---
+++ cell.py
@@ -0,0 +1,10 @@
+from PyQt5.QtWidgets import QWidget
+
+
+class Cell(QWidget):
+
+    def __init__(self, value=0):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
